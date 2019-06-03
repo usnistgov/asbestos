525a2857b4bca9f3b1b1eefb741f06479685b93c
bill <bmajur@gmail.com>
2019-06-03 10:47:20 -0400

    remove submodule

---
+++ asbestos-test-editor-service/src/main/java/gov/nist/asbestos/simapiTest/HelloWorldApplication.java
@@ -0,0 +1,36 @@
+package gov.nist.asbestos.simapiTest;
+
+import gov.nist.asbestos.simapiTest.health.TemplateHealthCheck;
+import gov.nist.asbestos.simapiTest.resources.HelloWorldResource;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+    public static void main(String[] args) throws Exception {
+        new HelloWorldApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "hello-world";
+    }
+
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+        // nothing to do yet
+    }
+
+    @Override
+    public void run(HelloWorldConfiguration configuration,
+                    Environment environment) {
+        final HelloWorldResource resource = new HelloWorldResource(
+                configuration.getTemplate(),
+                configuration.getDefaultName()
+        );
+        final TemplateHealthCheck healthCheck =
+                new TemplateHealthCheck(configuration.getTemplate());
+        environment.healthChecks().register("template", healthCheck);
+        environment.jersey().register(resource);
+    }
+}
