b4f46f98b8c45c23180b6bda84d239f981b7ba2e
bill <bmajur@gmail.com>
2019-10-02 07:44:34 -0400

    Work

---
+++ asbestos-view/src/components/ClientDetails.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        props: [
+            'sessionId', 'channelId', 'testCollection', 'testId'
+        ],
+        name: "ClientDetails"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- asbestos-view/src/components/TestOrEvalDetails.vue
+++ asbestos-view/src/components/TestOrEvalDetails.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div v-if="$store.state.testRunner.isClientTest">
-            <eval-details :sessionId="sessionId" :channelId="channelId" :testCollection="testCollection" :testId="testId"></eval-details>
+            <client-details :sessionId="sessionId" :channelId="channelId" :testCollection="testCollection" :testId="testId"></client-details>
         </div>
         <div v-else>
             <test-details :sessionId="sessionId" :channelId="channelId" :testCollection="testCollection" :testId="testId"></test-details>
@@ -11,13 +11,13 @@
 
 <script>
     import TestDetails from './TestDetails'
-    import EvalDetails from './EvalDetails'
+    import ClientDetails from './ClientDetails'
     export default {
         props: [
             'sessionId', 'channelId', 'testCollection', 'testId'
         ],
         components: {
-            TestDetails, EvalDetails
+            TestDetails, ClientDetails
         },
         name: "TestOrEvalDetails"
     }
