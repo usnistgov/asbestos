ad95ec9460f5b48322db30ace115c014e5ff7226
Bhaskarla <skb1@NIST.GOV>
2019-11-12 16:57:53 -0500

    Update comments

--- tomcat-dist-template/testing-tools/fhirtoolkit-base/bin/setenv.bat
+++ tomcat-dist-template/testing-tools/fhirtoolkit-base/bin/setenv.bat
@@ -1,5 +1,5 @@
 set TESTING_TOOLS=%CATALINA_HOME%\testing-tools
-set "CATALINA_OPTS=%CATALINA_OPTS% -DEXTERNAL_CACHE=%TESTING_TOOLS%\external_cache"
-REM If an alternate External Cache is desired, please use update the -DEXTERNAL_CACHE Java System Property above.
 set "CATALINA_OPTS=%CATALINA_OPTS% -DSERVICE_PROPERTIES=%TESTING_TOOLS%\service.properties"
+REM External Cache: If an alternate External Cache is desired, please use update the -DEXTERNAL_CACHE Java System Property below.
+set "CATALINA_OPTS=%CATALINA_OPTS% -DEXTERNAL_CACHE=%TESTING_TOOLS%\external_cache"
 
