f8742fde9f647af87e16e37ed3d167d0d450e3b2
Bhaskarla <skb1@NIST.GOV>
2019-12-02 16:23:38 -0500

    Use external service properties for Production package

--- asbestos-assembly/bundled-tomcat-9.0.26/Toolkits/FhirToolkit/bin/setenv.bat
+++ asbestos-assembly/bundled-tomcat-9.0.26/Toolkits/FhirToolkit/bin/setenv.bat
@@ -2,8 +2,7 @@ SET TOOLKITS_DIR=%CATALINA_HOME%\Toolkits
 
 REM Service Properties
 REM Enable the Java system property to use a specific property file
-REM SET "CATALINA_OPTS=%CATALINA_OPTS% -DSERVICE_PROPERTIES=%TOOLKITS_DIR%\service.properties"
-REM The default copy of the service properties is inside the FhirToolkit\webapps\WEB-INF\classes directory.
+SET "CATALINA_OPTS=%CATALINA_OPTS% -DSERVICE_PROPERTIES=%TOOLKITS_DIR%\FhirToolkit\conf\service.properties"
 
 REM External Cache
 REM If an alternate External Cache location is desired, please update the -DEXTERNAL_CACHE Java System Property below.
--- asbestos-assembly/bundled-tomcat-9.0.26/Toolkits/FhirToolkit/bin/setenv.sh
+++ asbestos-assembly/bundled-tomcat-9.0.26/Toolkits/FhirToolkit/bin/setenv.sh
@@ -2,8 +2,7 @@ TOOLKITS_DIR=$CATALINA_HOME/Toolkits
 
 # Service Properties
 # Enable the Java system property to use a specific property file
-# CATALINA_OPTS="$CATALINA_OPTS -DSERVICE_PROPERTIES=$TOOLKITS_DIR/service.properties"
-# The default copy of the service properties is inside the FhirToolkit\webapps\WEB-INF\classes directory.
+CATALINA_OPTS="$CATALINA_OPTS -DSERVICE_PROPERTIES=$TOOLKITS_DIR/FhirToolkit/conf/service.properties"
 
 # External Cache
 # If an alternate External Cache location is desired, please update the -DEXTERNAL_CACHE Java System Property below.
