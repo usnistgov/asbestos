0df262aeb732de2571389187f43772441cb2ad35
bill <bmajur@gmail.com>
2020-04-03 12:32:25 -0400

    UI upgrades to load modular logs for server tests

---
+++ asbestos-view/src/components/testRunner/ImportTestDetails.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div v-if="script.action">
+            <div v-for="(action, actioni) in script.action" class="action-margins"
+                 :key="'Action' + actioni">
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    //import ActionDetails from './ActionDetails'
+    export default {
+        props: [
+            'script',   // moduleScript.test
+            'report',   // moduleReport.test
+        ],
+        components: {
+      //      ActionDetails
+        },
+        name: "ImportTestDetails"
+    }
+</script>
+
+<style scoped>
+
+</style>
