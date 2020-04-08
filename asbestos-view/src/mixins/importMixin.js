f11c2c69b173d82af653f05a9e46954f13e30e30
bill <bmajur@gmail.com>
2020-04-08 06:49:15 -0400

    TestScript import now handles module naming through the TestReport - using a component multiple times in a TestScript should work.

--- asbestos-view/src/mixins/importMixin.js
+++ asbestos-view/src/mixins/importMixin.js
@@ -7,47 +7,12 @@ export default {
         }
     },
     methods: {
-        getComponentName(url) {
-            const dirs = url.split(path.sep)
-            if (dirs.length === 0) return url
-            const nameAndExtension = dirs[dirs.length - 1]
-            const parts = nameAndExtension.split('.')
-            if (parts.length === 0) return nameAndExtension
-            return parts[0]
-        },
-        // setImportComponentName(actionScript) {
-        //     const theImport = this.getImportFromScript(actionScript)
-        //     if (!theImport) return null
-        //     let componentName = null
-        //     if (!theImport.modifierExtension) return null
-        //     theImport.modifierExtension.forEach(extension => {
-        //         if (extension.url === 'component') {
-        //             const value = extension.valueString
-        //             componentName = this.getComponentName(value)
-        //         }
-        //     })
-        //     if (componentName)
-        //         this.componentName = this.currentTest + path.sep + componentName
-        //     else
-        //         this.componentName = null
-        //     return this.componentName
-        // },
         setImportComponentName(actionReport) {
             if (actionReport === null) return null;
             const moduleId = this.getModuleIdFromReport(actionReport);
             if (!moduleId) return null;
             this.componentName = this.currentTest + path.sep + moduleId;
             return this.componentName;
-        },
-        getImportFromScript(actionScript) {
-            if (!actionScript.operation) return null
-            if (!actionScript.operation.modifierExtension) return null
-            let theImport = null
-            actionScript.operation.modifierExtension.forEach(extension => {
-                if (extension.url === 'https://github.com/usnistgov/asbestos/wiki/TestScript-Import')
-                    theImport = extension
-            })
-            return theImport
         },
         getModuleIdFromReport(actionReport) {
             if (!actionReport.operation) return null;
