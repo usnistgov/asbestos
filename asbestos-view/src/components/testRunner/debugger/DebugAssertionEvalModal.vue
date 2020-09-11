46b377d4caeebb00326707c1874390bb597a127f
Bhaskarla <skb1@NIST.GOV>
2020-09-11 12:45:25 -0400

    Make the background for the modal mask darker

--- asbestos-view/src/components/testRunner/debugger/DebugAssertionEvalModal.vue
+++ asbestos-view/src/components/testRunner/debugger/DebugAssertionEvalModal.vue
@@ -251,7 +251,7 @@
         left: 0;
         width: 100%;
         height: 100%;
-        /*background-color: rgba(0, 0, 0, .5);*/
+        background-color: rgba(0, 0, 0, .3);
         /*transition: opacity 500ms ease;*/
     }
 
