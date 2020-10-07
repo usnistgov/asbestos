53a397073df73e628d156d709d367f92c392374a
Bhaskarla <skb1@NIST.GOV>
2020-10-07 17:58:59 -0400

    modalMode Vue property to make Inspector components work inside a modal dialog. Note: Inspector PDB Validations link uses TestRunner to run a test, this will disturb existing test being run/debugged.

--- asbestos-view/src/components/testRunner/EvalDetails.vue
+++ asbestos-view/src/components/testRunner/EvalDetails.vue
@@ -114,9 +114,11 @@
                 return assert.description === undefined ? "" : assert.description
             },
             async loadTestScript() {
-                this.$store.commit('setTestCollectionName', this.testCollection)
-                await this.$store.dispatch('loadTestScripts', [this.testId]);
-                this.script = this.$store.state.testRunner.testScripts[this.testId]
+                if (this.modalMode === undefined || this.modalMode==='') {
+                    this.$store.commit('setTestCollectionName', this.testCollection)
+                    await this.$store.dispatch('loadTestScripts', [this.testId]);
+                    this.script = this.$store.state.testRunner.testScripts[this.testId]
+                }
             },
             loadTestReport() {
                 const reports = this.$store.state.testRunner.clientTestResult[this.testId]
@@ -193,7 +195,7 @@
         },
         mixins: [ errorHandlerMixin, colorizeTestReports ],
         props: [
-            'sessionId', 'channelId', 'testCollection', 'testId', 'eventId', 'runEval', 'noInspectLabel',
+            'sessionId', 'channelId', 'testCollection', 'testId', 'eventId', 'runEval', 'noInspectLabel', 'modalMode'
         ],
         components: {
             //EvalReportAssert
