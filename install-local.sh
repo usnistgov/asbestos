3228e6fab5b504097ad3ac42b19591ef0c2eb0ae
Bhaskarla <skb1@NIST.GOV>
2022-07-25 18:04:31 -0400

    Fix #205 WAR file extraction command in build script

--- install-local.sh
+++ install-local.sh
@@ -83,7 +83,7 @@ echo "EXPAND XDSTOOLS"
 cd $INSTALL/tomcat/Toolkits/XdsToolkit/webapps
 mkdir xdstools
 cd xdstools
-unzip -qq ../xdstools.war
+jar -xf ../xdstools.war
 
 echo "REMOVE XDSTOOLS.WAR"
 rm -f ../xdstools.war
